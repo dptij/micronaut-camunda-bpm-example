58cfb4c1559a587ff80111d0ef5c2696f4a76575
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-01-27 11:49:29 +0100

    Initial commit.

---
+++ src/main/java/info/novatec/micronaut/camunda/bpm/example/CamundaController.java
@@ -0,0 +1,51 @@
+package info.novatec.micronaut.camunda.bpm.example;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.repository.ResourceDefinition;
+
+import java.util.stream.Collectors;
+
+@Controller("/camunda")
+public class CamundaController {
+
+    private final ProcessEngine processEngine;
+
+    private final RepositoryService repositoryService;
+
+    private final RuntimeService runtimeService;
+
+    public CamundaController(ProcessEngine processEngine, RepositoryService repositoryService, RuntimeService runtimeService) {
+        this.processEngine = processEngine;
+        this.repositoryService = repositoryService;
+        this.runtimeService = runtimeService;
+    }
+
+    @Get("/name")
+    @Produces(MediaType.TEXT_PLAIN)
+    @ExecuteOn(TaskExecutors.IO)
+    public String name() {
+        return processEngine.getName();
+    }
+
+    @Get("/definitions")
+    @Produces(MediaType.TEXT_PLAIN)
+    @ExecuteOn(TaskExecutors.IO)
+    public String definitions() {
+        return repositoryService.createProcessDefinitionQuery().list().stream()
+                .map(ResourceDefinition::getKey)
+                .collect(Collectors.joining());
+    }
+
+    @Post("/hello-world-process")
+    @ExecuteOn(TaskExecutors.IO)
+    public String startHelloWorldProcess() {
+        return runtimeService.startProcessInstanceByKey("HelloWorld").getId();
+    }
+
+}
---
+++ src/test/java/info/novatec/micronaut/camunda/bpm/example/CamundaControllerTest.java
@@ -0,0 +1,35 @@
+package info.novatec.micronaut.camunda.bpm.example;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+class CamundaControllerTest {
+
+    @Inject
+    @Client("/camunda")
+    RxHttpClient client;
+
+    @Test
+    void name() {
+        HttpRequest<String> request = HttpRequest.GET("/name");
+        String body = client.toBlocking().retrieve(request);
+
+        assertEquals("default", body);
+    }
+
+    @Test
+    void definitions() {
+        HttpRequest<String> request = HttpRequest.GET("/definitions");
+        String body = client.toBlocking().retrieve(request);
+
+        assertEquals("HelloWorld", body);
+    }
+}
